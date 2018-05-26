92eb369ce6de1d145c2bc6c51274731c9dadc523
brettasmi <brett.a.smi@gmail.com>
2018-05-26 16:48:05 -0700

    add DummiesEncoder class

--- regression_tools/dftransformers.py
+++ regression_tools/dftransformers.py
@@ -129,3 +129,144 @@ class Intercept(TransformerMixin):
             return pd.Series(np.ones(X.shape[0]),
                              index=X.index, name="intercept")
         return np.ones(X.shape[0])
+
+class DummiesEncoder(TransformerMixin):
+    """Makes dummy variables for all categorical columns in a dataframe.
+    """
+    def __init__(self, idxs="all", levels=None, less=None, drop=True):
+        """Instatiates transformer
+        Parameters:
+        idxs ("all" or array-like): column indicies to generate dummies
+            array-like str column names for pd.DataFrame
+            array-like int column indicies for np.array
+            or "all" for all columns regardless of data object
+            use "all" for pd.Series
+
+        levels (mapping): dict of col_index:list of str or int for levels to
+            dummify or None to encode all levels for all features
+            Example: df = pd.DataFrame({"color":"blue", "orange", "red"},
+                                        "flavor": "berry", "orange", "cherry"})
+                     sd = SmartDummies(levels={"color":["red","blue],
+                                               "flavor":["cherry", "berry"]})
+                     sd.fit(df)
+                     sd.transform(df)
+                     >>>"color_red" "color_blue" "flavor_cherry" "flavor_berry"
+                         0           1             0              1
+                         0           0             0              0
+                         1           0             1              0
+            important: explicitly defining levels overrides num_dummies below,
+            i.e. if you want explicitly set the levels with n-1 total levels,
+            make sure to do that with this option.
+        less (int > 0): for given n feature levels, n - less to encode.
+            Useful for models that require n-1 or fewer levels to avoid
+            gradient descent problems. Use None for all levels.
+
+        drop (bool): whether to drop columns that will be dummified
+        """
+        self.idxs = idxs
+        self.levels = levels
+        if less == None:
+            self.less = None
+        else:
+            self.less = -(less)
+        self.drop = drop
+    def fit(self, X):
+        """
+        """
+        # get all indices if not user-defined
+        if self.idxs == "all":
+            if isinstance(X, pd.DataFrame):
+                self.idxs = [col for col in X.columns]
+            elif isinstance(X, pd.Series):
+                self.idxs = [X.name]
+            elif isinstance(X, np.ndarray):
+                self.idxs = [i for i in range(X.shape[1])]
+            else:
+                raise TypeError("Expected pd.DataFrame, pd.Series, or np.ndarray")
+
+        # get levels if not user-defined
+        if not self.levels:
+            self.levels = {}
+            for index in self.idxs:
+                if isinstance(X, pd.DataFrame):
+                    self.levels[index] = self._get_levels(X[index])
+                elif isinstance(X, pd.Series):
+                    self.levels[index] = self._get_levels(X)
+                else:
+                    self.levels[index] = self._get_levels(X[:,index])
+        return self
+    def transform(self, X):
+        """
+        """
+
+        # handle pd.DataFrame
+        if isinstance(X, pd.DataFrame):
+            colnames = []
+            dum_arr = self._make_dum_array(X)
+            for key in self.levels:
+                colnames.extend(self._make_colnames(key, self.levels[key]))
+            dum_df = pd.DataFrame(dum_arr, columns=colnames)
+            if self.drop:
+                temp_df = X[[col for col in X.columns if col not in self.idxs]]
+                return pd.concat((temp_df, dum_df), axis=1)
+            else:
+                return pd.concat((X, dum_df), axis=1)
+        # handle pd.Series
+        elif isinstance(X, pd.Series):
+            colnames = []
+            values = np.array(self.levels[self.idxs[0]]) == X.values.reshape(-1,1)
+            if not self.drop:
+                values = np.concatenate((X.values.reshape(-1,1), values), axis=1)
+                colnames.append(X.name)
+                colnames.extend(self._make_colnames(X.name,
+                                                    self.levels[self.idxs[0]]))
+            else:
+                colnames = self._make_colnames(X.name, self.levels[self.idxs[0]])
+            return pd.DataFrame(values, columns=colnames)
+
+        # handle np.array
+        # check for correct dimensions here?
+        elif isinstance(X, np.ndarray):
+            # instantiate empty array of zeros to update with dummies
+            dum_arr = self._make_dum_array(X)
+
+            if self.drop:
+                non_dum_arr = X[:,[i for i in range(X.shape[1])
+                                   if i not in self.idxs]]
+                return np.concatenate((non_dum_arr, dum_arr), axis=1)
+            else:
+                return np.concatenate((X, dum_arr), axis=1)
+
+        else:
+            raise TypeError("Expected pd.DataFrame, pd.Series, or np.ndarray")
+
+
+    def _get_levels(self, arr):
+        """Returns unique levels in array-like arr"""
+        return list(set(arr))[:self.less]
+
+    def _make_colnames(self, prefix, levels):
+        """Returns list of str column names
+        Parameters:
+        prefix (str): prefix to appear in all column names
+        levels (list of str): list of suffixes to append to individual columns
+
+        Returns:
+        list of str in form prefix_level
+        """
+        return [f"{prefix}_{level}" for level in levels]
+
+    def _make_dum_array(self, X):
+        """Returns np.array of dummied variables"""
+        dum_col_cnt = sum(len(val_list) for val_list in self.levels.values())
+        dum_arr = np.zeros((X.shape[0], dum_col_cnt), dtype=np.int8)
+        curr_col_idx = 0
+        for key in self.levels:
+            if isinstance(X, pd.DataFrame):
+                curr_vals = np.array(X[key].values).reshape(-1,1)
+            else:
+                curr_vals = X[:,key].reshape(-1,1)
+            # broadcast boolean check to fill array
+            (dum_arr[:,curr_col_idx:curr_col_idx + len(self.levels[key])]) += (np.array(self.levels[key]) == curr_vals)
+            curr_col_idx += len(self.levels[key])
+        return dum_arr
